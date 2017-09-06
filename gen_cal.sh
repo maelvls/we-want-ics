1922faa99e53301ef84d791ba5a707555bc8cc28
Maël Valais <mael.valais@gmail.com>
2017-09-06 16:04:04 +0200

    fixed courses

--- gen_cal.sh
+++ gen_cal.sh
@@ -5,7 +5,7 @@
 #
 #
 
-ICS=/usr/local/WWW-personnel/Mael.Valais/ics
+ICS=.
 if [ ! -d "$ICS" ]; then mkdir "$ICS"; fi
 
 pip3 -q install icalendar lxml docopt || exit 1
@@ -16,7 +16,10 @@ EOF
 
 curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_SIAME/g241742.xml
 curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IHM/g241638.xml
-$PWD/celcat_to_ics.py g*.xml -r "TPA:compilation" -o "$ICS"/mael.ics
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L2/L2_CUPGE/g224943.xml
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IARF/g241496.xml
+$PWD/celcat_to_ics.py g*.xml -r "M1 INF-IARF s1 TPA,M1 INF-IHM s1 TPA:compilation+M1 INF-SIAME s1 TPA,M1 INF-IHM s1 TPA:Algorithmique avancée+L2 CUPGE s2 TPA:Informatique" -o "$ICS"/mael.ics
 
 # Check the permissions
 find "$ICS" \( -type d -exec chmod ugo+rx {} \; \) -o \( -type f -exec chmod ugo+r {} \; \)
+

d4edc975182266af0acafdf2fe702e112972ff6e
Maël Valais <mael.valais@gmail.com>
2018-01-10 15:33:55 +0100

    update

--- gen_cal.sh
+++ gen_cal.sh
@@ -8,7 +8,7 @@
 # Exit on any error in the script
 set -e
 
-ICS=.
+ICS=/usr/local/WWW-personnel/Mael.Valais/ics/
 if [ ! -d "$ICS" ]; then mkdir "$ICS"; fi
 
 pip3 -q install icalendar lxml docopt || exit 1
@@ -24,12 +24,21 @@ cd $(dirname "$0")
 #    https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_SIAME/g241742.xml
 #    https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_SIAME/finder.html
 
-curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_SIAME/g241742.xml
-curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IHM/g241638.xml
-curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_DL/g241675.xml
-curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IARF/g241496.xml
-curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_PS/g220501.xml
-$PWD/celcat_to_ics.py g*.xml -r "M1 INF-IARF s1 TPA,M1 INF-IHM s1 TPA:compilation+M1 INF-SIAME s1 TPA,M1 INF-DL s1 TPA11:Algorithmique avancée+L1 PS s1 TPA2:EPMAS1D1 - Informatique" -o "$ICS"/mael.ics -v
+#curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_SIAME/g241742.xml
+#curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IHM/g241638.xml
+#curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_DL/g241675.xml
+#curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/M1/M1_INF_IARF/g241496.xml
+#curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_PS/g220501.xml
+
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/g254186.xml
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/g254187.xml
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/g254189.xml
+curl -s -O https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/g254190.xml
+# To search the courses with 'Logique 1' inside:
+# curl https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/finder.xml | grep href= | cut -d'"' -f2 | while read l; echo $l; do curl -s https://edt.univ-tlse3.fr/FSI/2017_2018/L1/L1_SF/$l | grep EPINF2C1; done
+
+$PWD/celcat_to_ics.py g*.xml -r \
+	"L1 PM s2 TDA1,L1 PM s2 TDA2,L1 PM s2 TDA4,L1 PM s2 TDA5:Logique 1" -o "$ICS"/mael.ics -v
 
 # Check the permissions
 find "$ICS" \( -type d -exec chmod ugo+rx {} \; \) -o \( -type f -exec chmod ugo+r {} \; \)
